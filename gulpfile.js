a79a67bec5804cdce85ce7c7362dbb4a09a2b76a
F0Dev <Fabio-Ottaviani-Dev@users.noreply.github.com>
2017-11-13 14:53:46 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
+var gulp      = require('gulp');
+var phpspec   = require('gulp-phpspec');
+var run       = require('gulp-run');
+var notify    = require('gulp-notify');
 
-var gulp    = require('gulp');
-var phpspec = require('gulp-phpspec');
-var run     = require('gulp-run');
-var notify  = require('gulp-notify');
+// single
+// var src       = ' ../project-dir/spec/singleFile.php';
+
+// all
+var src       = '';
+
+var run       = './bin/phpspec run' + src;
+var options   = {verbose: 'v', notify: true, clear: true, formatter: 'pretty'};
+
 
 gulp.task('test', function() {
-  var options = {verbose: 'v', notify: true, clear: true, formatter: 'pretty'};
-  gulp.src('spec/**/*.php')
-  .pipe(phpspec('./bin/phpspec run', options))
+  gulp.src('')
+  .pipe(phpspec(run, options))
   .on('error', notify.onError({
     title: 'Crap',
     message: 'Your tests failed!',
     icon: __dirname + '/_icons/ko.png'
-    //,sound: 'Basso'
+    ,sound: 'Basso'
   }))
   .pipe(notify({
     title: 'Success',
